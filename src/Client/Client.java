f9f1b94eb5a391430812602c89bc0f9146aa946b
Job de Nooijer <j.n.d.nooijer@student.tue.nl>
2018-04-26 16:42:47 +0200

    First commit

---
+++ src/Client/Client.java
@@ -0,0 +1,7 @@
+package Client;
+
+/**
+ * Created by Job de Nooijer on 26/04/2018.
+ */
+public class Client {
+}
---
+++ src/Server/Server.java
@@ -0,0 +1,7 @@
+package Server;
+
+/**
+ * Created by Job de Nooijer on 26/04/2018.
+ */
+public class Server {
+}
